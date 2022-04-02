d785986095d2afd70dfd112f3aab3bf70256caae
Allanfers <60447483+Allanfers@users.noreply.github.com>
2022-04-02 13:53:15 -0300

    teste sql

--- gcp-main.tf
+++ gcp-main.tf
@@ -6,3 +6,18 @@ resource "google_artifact_registry_repository" "spotmusic-grupo09-artf-repo" {
   description = "Imagens Docker"
   format = "DOCKER"
 }
+
+module "cloudsql-mysql" {
+  source = "git::ssh://git@github.com:google-terraform-modules/terraform-google-cloudsql.git"
+
+  general = {
+    name       = "mydatabase"
+    env        = "dev"
+    region     = "us-west1"
+    db_version = "MYSQL_5_6"
+  }
+
+  instance = {
+    zone = "b"
+  }
+}
